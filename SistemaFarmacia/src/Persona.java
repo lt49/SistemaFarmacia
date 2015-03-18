e11f162e169730f2ce56ba1468c29d026f34fc05
docente04 <docente04@TPP-LAB04-02.TPP.UPEU.EDU.PE>
2015-03-18 11:10:04 -0500

    Se creo la clase Persona y se subio por primera vez

---
+++ SistemaFarmacia/src/Persona.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author docente04
+ */
+public class Persona {
+    
+    String nombre;
+    String apellidoMat;
+    String apellidoPat;
+    int dni;
+    int celular;
+    String direccion;
+    String email;
+
+    public Persona(String nombre, String apellidoMat, String apellidoPat, int dni, int celular, String direccion, String email) {
+        this.nombre = nombre;
+        this.apellidoMat = apellidoMat;
+        this.apellidoPat = apellidoPat;
+        this.dni = dni;
+        this.celular = celular;
+        this.direccion = direccion;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoMat() {
+        return apellidoMat;
+    }
+
+    public void setApellidoMat(String apellidoMat) {
+        this.apellidoMat = apellidoMat;
+    }
+
+    public String getApellidoPat() {
+        return apellidoPat;
+    }
+
+    public void setApellidoPat(String apellidoPat) {
+        this.apellidoPat = apellidoPat;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+}
