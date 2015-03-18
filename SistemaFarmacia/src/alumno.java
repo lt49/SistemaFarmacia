deceea907ddca44439302650d36273625db5fe65
Eliacer Fernandez <Eliacer Fernandez@192.168.1.96>
2015-03-18 11:11:00 -0500

    se creo la clase alumno

---
+++ SistemaFarmacia/src/alumno.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Eliacer Fernandez
+ */
+public class alumno {
+    
+    int codigoUniv;
+    String escuelaAcademica;
+    String facultad;
+    String  salon;
+    
+    
+    
+    public alumno() {
+        
+    }
+
+    public int getCodigoUniv() {
+        return codigoUniv;
+    }
+
+    public void setCodigoUniv(int codigoUniv) {
+        this.codigoUniv = codigoUniv;
+    }
+
+    public String getEscuelaAcademica() {
+        return escuelaAcademica;
+    }
+
+    public void setEscuelaAcademica(String escuelaAcademica) {
+        this.escuelaAcademica = escuelaAcademica;
+    }
+
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+
+    public String getSalon() {
+        return salon;
+    }
+
+    public void setSalon(String salon) {
+        this.salon = salon;
+    }
+
+    
+}
